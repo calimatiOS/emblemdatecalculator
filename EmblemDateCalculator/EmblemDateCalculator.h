b00c39fbf088c4492e930c8ef7810453f0324585
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-02-16 23:02:19 -0500

    Initial Commit

---
+++ EmblemDateCalculator/EmblemDateCalculator.h
@@ -0,0 +1,19 @@
+//
+//  EmblemDateCalculator.h
+//  EmblemDateCalculator
+//
+//  Created by Ricardo Herrera Petit on 2/16/20.
+//  Copyright © 2020 Ricardo Herrera Petit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmblemDateCalculator.
+FOUNDATION_EXPORT double EmblemDateCalculatorVersionNumber;
+
+//! Project version string for EmblemDateCalculator.
+FOUNDATION_EXPORT const unsigned char EmblemDateCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmblemDateCalculator/PublicHeader.h>
+
+
