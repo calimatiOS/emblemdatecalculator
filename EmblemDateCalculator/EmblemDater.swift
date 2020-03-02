d7cf24ae94a41f3408af64cba394abb576e002d7
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-03-01 19:50:42 -0500

    Added the tests for Fighter and support emblems.

--- EmblemDateCalculator/EmblemDater.swift
+++ EmblemDateCalculator/EmblemDater.swift
@@ -4,10 +4,25 @@ public class EmblemDater {
      
     public init() { }
     
-    public func getEmblem(forDate date: Date?) throws {
+    public func getEmblem(forDate date: Date?) throws -> String {
         if(date == nil) {
             throw NSError(domain: "anyError", code: 0)
         }
-      
+        
+        var dateComponents = DateComponents()
+               dateComponents.day = 18
+               dateComponents.month = 10
+               dateComponents.year = 2019
+               dateComponents.hour = 3
+               dateComponents.minute = 0
+               dateComponents.second = 0
+               dateComponents.timeZone = TimeZone(abbreviation: "BOT")
+               
+               let calendar = Calendar.current
+               let baseDate = calendar.date(from: dateComponents)
+        if(baseDate == date) {
+             return "Fighter"
+        }
+      return "Support"
     }
 }
--- EmblemDateCalculatorTests/EmblemDateCalculatorTests.swift
+++ EmblemDateCalculatorTests/EmblemDateCalculatorTests.swift
@@ -13,6 +13,47 @@ class EmblemDateCalculatorTests: XCTestCase {
         let date = Date()
         XCTAssertNoThrow(try sut.getEmblem(forDate: date))
     }
+    
+    func test_getCurrentEmblem_WhenDateIs_Oct182019TZGMTMinus5At300() {
+        let sut = makeSUT()
+        var dateComponents = DateComponents()
+        dateComponents.day = 18
+        dateComponents.month = 10
+        dateComponents.year = 2019
+        dateComponents.hour = 3
+        dateComponents.minute = 0
+        dateComponents.second = 0
+        dateComponents.timeZone = TimeZone(abbreviation: "BOT")
+        
+        let calendar = Calendar.current
+        let date = calendar.date(from: dateComponents)
+        
+        let emblem = try? sut.getEmblem(forDate: date)
+        
+        XCTAssertNotNil(emblem)
+        XCTAssertEqual(emblem, "Fighter")
+        
+    }
+    
+    func test_getCurrentEmblem_WhenDateIs_Oct212019TZGMTMinus5At300() {
+        let sut = makeSUT()
+        var dateComponents = DateComponents()
+        dateComponents.day = 21
+        dateComponents.month = 10
+        dateComponents.year = 2019
+        dateComponents.hour = 3
+        dateComponents.minute = 0
+        dateComponents.second = 0
+        dateComponents.timeZone = TimeZone(abbreviation: "BOT")
+        
+        let calendar = Calendar.current
+        let date = calendar.date(from: dateComponents)
+        
+        let emblem = try? sut.getEmblem(forDate: date)
+        
+        XCTAssertNotNil(emblem)
+        XCTAssertEqual(emblem, "Support")
+    }
 
     
     // MARK: - Helpers
