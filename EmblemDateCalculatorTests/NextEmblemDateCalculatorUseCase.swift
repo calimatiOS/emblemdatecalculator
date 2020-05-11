bd176fef50ab971774848290d0a3041ad6f256f4
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-05-10 19:06:13 -0500

    Refactor the test code for readability and reusability.

--- EmblemDateCalculatorTests/NextEmblemDateCalculatorUseCase.swift
+++ EmblemDateCalculatorTests/NextEmblemDateCalculatorUseCase.swift
@@ -5,34 +5,33 @@ class NextEmblemDateCalculatorUseCase: XCTestCase {
 
     func test_WhenDateIsOct182019_WhenIsNextSupportDate_ReturnsOct212019() {
         
-        //Arrange
-        let sut = EmblemDater()
-        let currentDate = sut.getDate()
-        let emblemToBeCalculated = Emblem.Support
-        let expectedDateEmblem = sut.getDate(forDay: 21, andMonth: 10, andYear: 2019)
-        
-        //Act
-        let resultEmblemDate = sut.getNextAvailableDate(for: emblemToBeCalculated, inCurrentDate: currentDate)
-        
-        //Assert
-        XCTAssertEqual(expectedDateEmblem, resultEmblemDate)
+        AssertThatForBaseDateThe(calculatedEmblem: .Support, expectedDay: 21, expectedMonth: 10, expectedYear: 2019)
+      
         
     }
     
     func test_WhenDateIsOct182019_WhenIsNextMarksmanDate_ReturnsOct242019() {
-           
-           //Arrange
-           let sut = EmblemDater()
-           let currentDate = sut.getDate()
-           let emblemToBeCalculated = Emblem.Marksman
-           let expectedDateEmblem = sut.getDate(forDay: 24, andMonth: 10, andYear: 2019)
-           
-           //Act
-           let resultEmblemDate = sut.getNextAvailableDate(for: emblemToBeCalculated, inCurrentDate: currentDate)
-           
-           //Assert
-           XCTAssertEqual(expectedDateEmblem, resultEmblemDate)
+        
+        AssertThatForBaseDateThe(calculatedEmblem: .Marksman, expectedDay: 24, expectedMonth: 10, expectedYear: 2019)
            
        }
+    
+    //MARK: -- Helpers
+    
+    func AssertThatForBaseDateThe(calculatedEmblem emblem:Emblem, expectedDay:Int, expectedMonth:Int, expectedYear:Int) {
+        
+        //Arrange
+        let sut = EmblemDater()
+                 let currentDate = sut.getDate()
+                 let emblemToBeCalculated = emblem
+                 let expectedDateEmblem = sut.getDate(forDay: expectedDay , andMonth: expectedMonth, andYear: expectedYear)
+                 
+                 //Act
+                 let resultEmblemDate = sut.getNextAvailableDate(for: emblemToBeCalculated, inCurrentDate: currentDate)
+                 
+                 //Assert
+                 XCTAssertEqual(expectedDateEmblem, resultEmblemDate)
+        
+    }
 
 }
