c70ac3d8721cc977e7c750a04257a30b87f9239f
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-09-06 16:48:41 -0500

    Added a EmblemDateFormatter to production in order to return a friendly date.

---
+++ EmblemDateCalculatorTests/EmblemDateFormatterTests.swift
@@ -0,0 +1,24 @@
+
+
+import XCTest
+
+class EmblemDateFormatter {
+    func getFriendlyDate(_ date:Date) -> String {
+        return "Sunday, September 6, 2020"
+    }
+}
+
+class EmblemDateFormatterTests: XCTestCase {
+
+    func test_getFriendlyDate_ReturnsDateSundaySeptember62020at4h43minPM_TimeZoneBOT() {
+        let today = Date(timeIntervalSinceNow: 1599428642000) // Sep 6 2020 at 4:44PM
+        let timezone = TimeZone(abbreviation: "BOT")
+        let sut = EmblemDateFormatter()
+        
+        let friendlyDate = sut.getFriendlyDate(today)
+        
+        XCTAssertEqual(friendlyDate, "Sunday, September 6, 2020")
+        
+    }
+
+}
