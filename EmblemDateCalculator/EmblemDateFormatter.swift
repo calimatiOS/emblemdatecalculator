5e5ca891c4f35b8af3b7c2fe2c1482bde4299f04
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-09-06 16:57:56 -0500

    Refactored EmblemDateFormatter to be in its own swift file.

---
+++ EmblemDateCalculator/EmblemDateFormatter.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public class EmblemDateFormatter {
+    public init() { }
+     public func getFriendlyDate(_ date:Date) -> String {
+        return "Sunday, September 6, 2020"
+    }
+}
--- EmblemDateCalculatorTests/EmblemDateFormatterTests.swift
+++ EmblemDateCalculatorTests/EmblemDateFormatterTests.swift
@@ -1,18 +1,10 @@
-
-
 import XCTest
-
-class EmblemDateFormatter {
-    func getFriendlyDate(_ date:Date) -> String {
-        return "Sunday, September 6, 2020"
-    }
-}
+import EmblemDateCalculator
 
 class EmblemDateFormatterTests: XCTestCase {
 
     func test_getFriendlyDate_ReturnsDateSundaySeptember62020at4h43minPM_TimeZoneBOT() {
         let today = Date(timeIntervalSinceNow: 1599428642000) // Sep 6 2020 at 4:44PM
-        let timezone = TimeZone(abbreviation: "BOT")
         let sut = EmblemDateFormatter()
         
         let friendlyDate = sut.getFriendlyDate(today)
