43464f92ccbb60756d5230f9273ae57be39af8c5
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-09-13 17:59:57 -0500

    Refactor code to make use of the friendly user date.

--- EmblemDateCalculator/EmblemDateFormatter.swift
+++ EmblemDateCalculator/EmblemDateFormatter.swift
@@ -3,6 +3,9 @@ import Foundation
 public class EmblemDateFormatter {
     public init() { }
      public func getFriendlyDate(_ date:Date) -> String {
-        return "Sunday, September 6, 2020 at 3:00 AM"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE, MMMM d, yyyy 'at' h:mm a"
+        let friendlydDate = dateFormatter.string(from: date)
+        return friendlydDate
     }
 }
--- EmblemDateCalculatorTests/EmblemDateFormatterTests.swift
+++ EmblemDateCalculatorTests/EmblemDateFormatterTests.swift
@@ -4,12 +4,12 @@ import EmblemDateCalculator
 class EmblemDateFormatterTests: XCTestCase {
 
     func test_getFriendlyDate_ReturnsDateSundaySeptember62020at4h43minPM_TimeZoneBOT() {
-        let today = Date(timeIntervalSinceNow: 1599428642000) // Sep 6 2020 at 4:44PM
+        let today = Date(timeIntervalSince1970: 1599428642) // Sep 6 2020 at 4:44PM
         let sut = EmblemDateFormatter()
         
         let friendlyDate = sut.getFriendlyDate(today)
         
-        XCTAssertEqual(friendlyDate, "Sunday, September 6, 2020 at 3:00 AM")
+        XCTAssertEqual(friendlyDate, "Sunday, September 6, 2020 at 4:44 PM")
         
     }
 
