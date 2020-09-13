b341403e98b8cb0bb884bf96f0b873f76ed61f11
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-09-13 18:11:36 -0500

    Added a different random date in order to test the friendly date behavior.

--- EmblemDateCalculatorTests/EmblemDateFormatterTests.swift
+++ EmblemDateCalculatorTests/EmblemDateFormatterTests.swift
@@ -3,7 +3,7 @@ import EmblemDateCalculator
 
 class EmblemDateFormatterTests: XCTestCase {
 
-    func test_getFriendlyDate_ReturnsDateSundaySeptember62020at4h43minPM_TimeZoneBOT() {
+    func test_getFriendlyDate_ReturnsDateSundaySeptember62020at4h43minPM() {
         let today = Date(timeIntervalSince1970: 1599428642) // Sep 6 2020 at 4:44PM
         let sut = EmblemDateFormatter()
         
@@ -12,5 +12,15 @@ class EmblemDateFormatterTests: XCTestCase {
         XCTAssertEqual(friendlyDate, "Sunday, September 6, 2020 at 4:44 PM")
         
     }
+    
+    func test_getFriendlyDate_ReturnsDateSundaySeptember132020at6h00minPM() {
+           let today = Date(timeIntervalSince1970: 1600038000) // Sep 13 2020 at 6:00PM
+           let sut = EmblemDateFormatter()
+           
+           let friendlyDate = sut.getFriendlyDate(today)
+           
+           XCTAssertEqual(friendlyDate, "Sunday, September 13, 2020 at 6:00 PM")
+           
+       }
 
 }
