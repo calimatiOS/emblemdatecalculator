24199ae472bab0a0609d42cd196e8e4b7196c8b3
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-09-27 18:09:19 -0500

    Created a second integration tests for Marksman emblem.

--- DaterFormatterIntegrationTests/DaterFormatterIntegrationTests.swift
+++ DaterFormatterIntegrationTests/DaterFormatterIntegrationTests.swift
@@ -12,5 +12,16 @@ class DaterFormatterIntegrationTests: XCTestCase {
         XCTAssertEqual(friendlyDate, "Monday, October 21, 2019 at 3:00 AM")
     }
   
+    func test_getFriendlyDateForMarksmanEmblem_WhenDateIsOct182019At3h00m_ThenShouldReturnOct242019FriendlyString() {
+        
+        let sut = EmblemDater()
+        let emblemFormatter = EmblemDateFormatter()
+        let currentDate = Date(timeIntervalSince1970: 1571385600)
+        let supportEmblemDate = sut.getNextAvailableDate(for: .Marksman, inCurrentDate: currentDate)
+        let friendlyDate = emblemFormatter.getFriendlyDate(supportEmblemDate)
+        XCTAssertEqual(friendlyDate, "Thursday, October 24, 2019 at 3:00 AM")
+    }
+    
+    
 
 }
