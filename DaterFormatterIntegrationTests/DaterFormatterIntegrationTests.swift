c0afec0f7aa0edfa9331423e468ea479f6591a18
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2020-09-27 18:20:37 -0500

    refactored integration tests in order to make code more clean.

--- DaterFormatterIntegrationTests/DaterFormatterIntegrationTests.swift
+++ DaterFormatterIntegrationTests/DaterFormatterIntegrationTests.swift
@@ -3,53 +3,43 @@ import EmblemDateCalculator
 
 class DaterFormatterIntegrationTests: XCTestCase {
 
+    
+     let sut = EmblemDater()
+     let emblemFormatter = EmblemDateFormatter()
+     let currentDate = Date(timeIntervalSince1970: 1571385600)
+    
+    
     func test_getFriendlyDateForSupportEmblem_WhenDateIsOct182019At3h00m_ThenShouldReturnOct212019InFriendlyString() {
-        let sut = EmblemDater()
-        let emblemFormatter = EmblemDateFormatter()
-        let currentDate = Date(timeIntervalSince1970: 1571385600)
-        let supportEmblemDate = sut.getNextAvailableDate(for: .Support, inCurrentDate: currentDate)
-        let friendlyDate = emblemFormatter.getFriendlyDate(supportEmblemDate)
-        XCTAssertEqual(friendlyDate, "Monday, October 21, 2019 at 3:00 AM")
+      
+        AssertFriendlyDateIs("Monday, October 21, 2019 at 3:00 AM", for: .Support)
     }
   
     func test_getFriendlyDateForMarksmanEmblem_WhenDateIsOct182019At3h00m_ThenShouldReturnOct242019FriendlyString() {
         
-        let sut = EmblemDater()
-        let emblemFormatter = EmblemDateFormatter()
-        let currentDate = Date(timeIntervalSince1970: 1571385600)
-        let supportEmblemDate = sut.getNextAvailableDate(for: .Marksman, inCurrentDate: currentDate)
-        let friendlyDate = emblemFormatter.getFriendlyDate(supportEmblemDate)
-        XCTAssertEqual(friendlyDate, "Thursday, October 24, 2019 at 3:00 AM")
+        AssertFriendlyDateIs("Thursday, October 24, 2019 at 3:00 AM", for: .Marksman)
     }
     
     func test_getFriendlyDateForTankEmblem_WhenDateIsOct182019At3h00m_ThenShouldReturnOct272019FriendlyString() {
         
-        let sut = EmblemDater()
-        let emblemFormatter = EmblemDateFormatter()
-        let currentDate = Date(timeIntervalSince1970: 1571385600)
-        let supportEmblemDate = sut.getNextAvailableDate(for: .Tank, inCurrentDate: currentDate)
-        let friendlyDate = emblemFormatter.getFriendlyDate(supportEmblemDate)
-        XCTAssertEqual(friendlyDate, "Sunday, October 27, 2019 at 3:00 AM")
+        AssertFriendlyDateIs("Sunday, October 27, 2019 at 3:00 AM", for: .Tank)
     }
     
     func test_getFriendlyDateForJungleEmblem_WhenDateIsOct182019At3h00m_ThenShouldReturnOct302019FriendlyString() {
         
-        let sut = EmblemDater()
-        let emblemFormatter = EmblemDateFormatter()
-        let currentDate = Date(timeIntervalSince1970: 1571385600)
-        let supportEmblemDate = sut.getNextAvailableDate(for: .Jungle, inCurrentDate: currentDate)
-        let friendlyDate = emblemFormatter.getFriendlyDate(supportEmblemDate)
-        XCTAssertEqual(friendlyDate, "Wednesday, October 30, 2019 at 3:00 AM")
+        AssertFriendlyDateIs("Wednesday, October 30, 2019 at 3:00 AM", for: .Jungle)
     }
     
     func test_getFriendlyDateForAssasinEmblem_WhenDateIsOct182019At3h00m_ThenShouldReturnNov022019FriendlyString() {
         
-        let sut = EmblemDater()
-        let emblemFormatter = EmblemDateFormatter()
-        let currentDate = Date(timeIntervalSince1970: 1571385600)
-        let supportEmblemDate = sut.getNextAvailableDate(for: .Assassin, inCurrentDate: currentDate)
+        AssertFriendlyDateIs("Saturday, November 2, 2019 at 3:00 AM", for: .Assassin)
+    }
+    
+    //Mark: Helper Methods
+    
+    fileprivate func AssertFriendlyDateIs(_ expectedDateString: String, for emblem:Emblem) {
+        let supportEmblemDate = sut.getNextAvailableDate(for: emblem, inCurrentDate: currentDate)
         let friendlyDate = emblemFormatter.getFriendlyDate(supportEmblemDate)
-        XCTAssertEqual(friendlyDate, "Saturday, November 2, 2019 at 3:00 AM")
+        XCTAssertEqual(friendlyDate, expectedDateString)
     }
     
     
